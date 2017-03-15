f1025e5d1615e3bea267a45ecb70fb30e5f44ac3
William Harirngton <kb0iic@gmail.com>
2017-03-15 13:16:29 -0500

    Add properties to Location class and make public.

--- Engine/Location.cs
+++ Engine/Location.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    class Location
+    public class Location
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
     }
 }
