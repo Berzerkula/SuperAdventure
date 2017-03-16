9cb0f8fc2d0f7a424cc35327213c2501f0b0a69e
William Harirngton <kb0iic@gmail.com>
2017-03-16 11:46:30 -0500

    Use the INotifyPropertyChanged interface to subscribe events on property change to update datagridviews.

--- Engine/InventoryItem.cs
+++ Engine/InventoryItem.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class InventoryItem
+    public class InventoryItem : INotifyPropertyChanged
     {
         public Item Details { get; set; }
         public int Quantity { get; set; }
@@ -15,6 +16,16 @@ namespace Engine
         {
             Details = details;
             Quantity = quantity;
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void OnPropertyChanged(string name)
+    {
+        if(PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
         }
     }
 }
