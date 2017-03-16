d319f94a24b325d0c147170b8774c6aeeadff7a0
William Harirngton <kb0iic@gmail.com>
2017-03-16 16:34:01 -0500

    Add Message and Add ExtraNewLine properties, make class public, and add the MessageEventArgs constructor.

--- Engine/MessageEventArgs.cs
+++ Engine/MessageEventArgs.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
-    class MessageEventArgs
+    public class MessageEventArgs
     {
+        public string Message { get; private set; }
+        public bool AddExtraNewLine { get; private set; }
+
+        public MessageEventArgs(string message, bool addExtraNewLine)
+        {
+            Message = message;
+            AddExtraNewLine = addExtraNewLine;
+        }
     }
 }
