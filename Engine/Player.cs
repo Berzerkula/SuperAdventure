118b01c78cbe2d1b3d52252df5773833829d8185
William Harirngton <kb0iic@gmail.com>
2017-03-16 13:50:19 -0500

    Add a new function to call when remove an item from the inventory. Utilize the RaiseIventoryChangedEvent when the inventory has changed.

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -209,6 +209,38 @@ namespace Engine
             }
         }
 
+        public void RemoveItemFromInventory(Item itemToRemove, int quantity = 1)
+        {
+            InventoryItem item = Inventory.SingleOrDeafult(ii => ii.Details.ID == itemToRemove.ID);
+
+            if (item == null)
+            {
+                // The item is not in the player's inventory, so ignore it.
+                // We might want to raise an error for this situation
+            }
+            else
+            {
+                // They have the item in their inventory, so decrease the quantity
+                item.Quantity -= quantity;
+
+                // Don't allow negative quantities.
+                // We might want to raise an error for this situation
+                if (item.Quantity < 0)
+                {
+                    item.Quantity = 0;
+                }
+
+                // If the quantity is zero, remove the item from the list
+                if (item.Quantity == 0)
+                {
+                    Inventory.Remove(item);
+                }
+
+                // Notify the UI that the inventory has changed
+                RaiseInventoryChangedEvent(itemToRemove);
+            }
+        }
+
         private void RaiseInventoryChangedEvent(Item item)
         {
             if (item is Weapon)
