fe71465f68cd0b9f269925ad87bc787a3810d437
William Harirngton <kb0iic@gmail.com>
2017-03-16 10:43:57 -0500

    Use binding and use INotifyPropertyChanged and subscribe the UI when a property change occurs.

--- Engine/LivingCreature.cs
+++ Engine/LivingCreature.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine
 {
-    public class LivingCreature
+    public class LivingCreature : INotifyPropertyChanged
     {
         public int CurrentHitPoints { get; set; }
         public int MaximumHitPoints { get; set; }
@@ -15,6 +16,16 @@ namespace Engine
         {
             CurrentHitPoints = currentHitPoints;
             MaximumHitPoints = maximumHitPoints;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
         }
     }
 }
