832844a6b211320a445426f827a40196981dcd8e
William Harrington <kb0iic@gmail.com>
2017-03-16 19:01:21 -0500

    Set properties for labels, datagrid views, and button.

--- SuperAdventure/TradingScreen.Designer.cs
+++ SuperAdventure/TradingScreen.Designer.cs
@@ -28,11 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lvlMyInventory = new System.Windows.Forms.Label();
+            this.lblVendorInventory = new System.Windows.Forms.Label();
+            this.dgvMyItems = new System.Windows.Forms.DataGridView();
+            this.dgvVendorItems = new System.Windows.Forms.DataGridView();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMyItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVendorItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lvlMyInventory
+            // 
+            this.lvlMyInventory.AutoSize = true;
+            this.lvlMyInventory.Location = new System.Drawing.Point(99, 13);
+            this.lvlMyInventory.Name = "lvlMyInventory";
+            this.lvlMyInventory.Size = new System.Drawing.Size(88, 17);
+            this.lvlMyInventory.TabIndex = 0;
+            this.lvlMyInventory.Text = "My Inventory";
+            // 
+            // lblVendorInventory
+            // 
+            this.lblVendorInventory.AutoSize = true;
+            this.lblVendorInventory.Location = new System.Drawing.Point(349, 13);
+            this.lblVendorInventory.Name = "lblVendorInventory";
+            this.lblVendorInventory.Size = new System.Drawing.Size(126, 17);
+            this.lblVendorInventory.TabIndex = 1;
+            this.lblVendorInventory.Text = "Vendor\'s Inventory";
+            // 
+            // dgvMyItems
+            // 
+            this.dgvMyItems.AllowUserToAddRows = false;
+            this.dgvMyItems.AllowUserToDeleteRows = false;
+            this.dgvMyItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMyItems.Location = new System.Drawing.Point(13, 43);
+            this.dgvMyItems.Name = "dgvMyItems";
+            this.dgvMyItems.ReadOnly = true;
+            this.dgvMyItems.RowTemplate.Height = 24;
+            this.dgvMyItems.Size = new System.Drawing.Size(240, 216);
+            this.dgvMyItems.TabIndex = 2;
+            // 
+            // dgvVendorItems
+            // 
+            this.dgvVendorItems.AllowUserToAddRows = false;
+            this.dgvVendorItems.AllowUserToDeleteRows = false;
+            this.dgvVendorItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVendorItems.Location = new System.Drawing.Point(276, 43);
+            this.dgvVendorItems.Name = "dgvVendorItems";
+            this.dgvVendorItems.ReadOnly = true;
+            this.dgvVendorItems.RowTemplate.Height = 24;
+            this.dgvVendorItems.Size = new System.Drawing.Size(240, 216);
+            this.dgvVendorItems.TabIndex = 3;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(441, 274);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // TradingScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "TradingScreen";
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(526, 302);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dgvVendorItems);
+            this.Controls.Add(this.dgvMyItems);
+            this.Controls.Add(this.lblVendorInventory);
+            this.Controls.Add(this.lvlMyInventory);
+            this.Name = "TradingScreen";
+            this.Text = "Trade";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMyItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVendorItems)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lvlMyInventory;
+        private System.Windows.Forms.Label lblVendorInventory;
+        private System.Windows.Forms.DataGridView dgvMyItems;
+        private System.Windows.Forms.DataGridView dgvVendorItems;
+        private System.Windows.Forms.Button btnClose;
     }
 }
