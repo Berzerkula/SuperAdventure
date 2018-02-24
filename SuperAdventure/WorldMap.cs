a2c306a56b99b218c5ec7807d938b9db9e19229e
kb0iic <kb0iic@berzerkula.org>
2018-02-23 19:44:57 -0600

    Add LocationsVisited conditionals based on visit status.

--- SuperAdventure/WorldMap.cs
+++ SuperAdventure/WorldMap.cs
@@ -9,7 +9,7 @@ namespace SuperAdventure
     {
         readonly Assembly _thisAssembly = Assembly.GetExecutingAssembly();
 
-        public WorldMap()
+        public WorldMap(Player player)
         {
             InitializeComponent();
 
@@ -18,30 +18,30 @@ namespace SuperAdventure
              * MessageBox.Show(toDisplay);
              */
 
-            SetImage(pic_0_0, "");
-            SetImage(pic_0_1, "");
-            SetImage(pic_0_2, "HerbalistsGarden");
-            SetImage(pic_0_3, "");
-            SetImage(pic_0_4, "");
-            SetImage(pic_0_5, "");
-            SetImage(pic_1_0, "");
-            SetImage(pic_1_1, "");
-            SetImage(pic_1_2, "HerbalistsHut");
-            SetImage(pic_1_3, "");
-            SetImage(pic_1_4, "");
-            SetImage(pic_1_5, "");
-            SetImage(pic_2_0, "FarmFields");
-            SetImage(pic_2_1, "Farmhouse");
-            SetImage(pic_2_2, "TownSquare");
-            SetImage(pic_2_3, "TownGate");
-            SetImage(pic_2_4, "Bridge");
-            SetImage(pic_2_5, "SpiderForest");
-            SetImage(pic_3_0, "");
-            SetImage(pic_3_1, "");
-            SetImage(pic_3_2, "Home");
-            SetImage(pic_3_3, "");
-            SetImage(pic_3_4, "");
-            SetImage(pic_3_5, "");
+            SetImage(pic_0_0, player.LocationsVisited.Contains(0)  ? "" : "FogLocation"););
+            SetImage(pic_0_1, player.LocationsVisited.Contains(1)  ? "" : "FogLocation"););
+            SetImage(pic_0_2, player.LocationsVisited.Contains(2)  ? "HerbalistsGarden" : "FogLocation");
+            SetImage(pic_0_3, player.LocationsVisited.Contains(3)  ? "" : "FogLocation"););
+            SetImage(pic_0_4, player.LocationsVisited.Contains(4)  ? "" : "FogLocation"););
+            SetImage(pic_0_5, player.LocationsVisited.Contains(5)  ? "" : "FogLocation"););
+            SetImage(pic_1_0, player.LocationsVisited.Contains(6)  ? "" : "FogLocation"););
+            SetImage(pic_1_1, player.LocationsVisited.Contains(7)  ? "" : "FogLocation"););
+            SetImage(pic_1_2, player.LocationsVisited.Contains(8)  ? "HerbalistsHut" : "FogLocation");
+            SetImage(pic_1_3, player.LocationsVisited.Contains(9)  ? "" : "FogLocation"););
+            SetImage(pic_1_4, player.LocationsVisited.Contains(10) ? "" : "FogLocation"););
+            SetImage(pic_1_5, player.LocationsVisited.Contains(11) ? "" : "FogLocation"););
+            SetImage(pic_2_0, player.LocationsVisited.Contains(12) ? "FarmFields" : "FogLocation");
+            SetImage(pic_2_1, player.LocationsVisited.Contains(13) ? "Farmhouse" : "FogLocation");
+            SetImage(pic_2_2, player.LocationsVisited.Contains(14) ? "TownSquare" : "FogLocation");
+            SetImage(pic_2_3, player.LocationsVisited.Contains(15) ? "TownGate" : "FogLocation");
+            SetImage(pic_2_4, player.LocationsVisited.Contains(16) ? "Bridge" : "FogLocation");
+            SetImage(pic_2_5, player.LocationsVisited.Contains(17) ? "SpiderForest" : "FogLocation");
+            SetImage(pic_3_0, player.LocationsVisited.Contains(18) ? "" : "FogLocation"););
+            SetImage(pic_3_1, player.LocationsVisited.Contains(19) ? "" : "FogLocation"););
+            SetImage(pic_3_2, player.LocationsVisited.Contains(20) ? "Home" : "FogLocation");
+            SetImage(pic_3_3, player.LocationsVisited.Contains(21) ? "" : "FogLocation"););
+            SetImage(pic_3_4, player.LocationsVisited.Contains(22) ? "" : "FogLocation"););
+            SetImage(pic_3_5, player.LocationsVisited.Contains(23) ? "" : "FogLocation"););
         }
 
         private void SetImage(PictureBox pictureBox, string imageName)
